e67d5df107b9fd723c73f2d28ccabe88b79b46c9
Jordan Apgar <s0626532@monmouth.edu>
2015-02-17 19:00:39 -0500

    updated some comments and formatting for the calculator

--- hw/hw1/calculator.php
+++ hw/hw1/calculator.php
@@ -1,8 +1,13 @@
 <html>
+    <!--
+        Jordan Apgar
+        s0626532@monmouth.edu
+        CS 628 - HW 1 - Spring 2015
+    -->
     <head>
-        <title>CS628</title>
+        <title>Calculator</title>
     </head>
-    <!-- body is a keyword -->
+    <!-- body is a keyword in the style -->
     <!-- don't put comments inside the style -->
     <style>        
         body {background-color: grey;}
@@ -10,24 +15,19 @@
         #header {height: 70px; line-height: 70px; background-color: blue; color: white;}
         #main {background-color: white; height: 300px}
         #footer {height: 30px; line-height: 30px; background-color: blue; color: white;}
-        table { padding-left: 200px;}
+        table { padding-left: 300px;}
     </style> <!-- height == line-height vertically centers the text -->
     <body>
       <div id="container">
         <div id="header" >
             <!-- inside the header you could also use
             style="padding:20px 100px" -->
-            <h1 align="center"> Calculator </h1>
+            <h1 align="center"> A Simple Calculator </h1>
         </div>
         
         <div id="main">
-            <!-- we want to create a form here -->
-            <!-- method how to transfer data to server
-                 post = usually for forms - more secure
-                 get = get adds data to URL so it is less secure
-                 -->
             <form action="" method="POST">
-                <table style = "padding-top: 10px;">
+                <table style = "padding-top: 50px;">
                     <tr>
                         <td><?php
                           $first = (isset($_POST['first']) ? $_POST['first'] : '');
@@ -69,7 +69,7 @@
                           echo "</select>";
                         ?></td>
                         <td><input type="submit" name="button" value="="/></td>
-                        <td>
+                        <td> <!-- result area so handle forms POSTing here -->
                             <?php
                                 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     $first = (isset($_POST['first']) ? $_POST['first'] : '');
@@ -98,6 +98,9 @@
                     </tr>
                 </table>
             </form>
+        </div>
+        <div id="footer">
+            <h5 align="center">Copyright 2015 Monmouth University</h5>
         </div>
       </div>
     </body>
